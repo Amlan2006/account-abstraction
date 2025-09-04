79319ae4ff8f22a61820e9fdc427b288646b64d3
Amlan2006 <amlanroy2020@gmail.com>
2025-09-04 20:31:12 +0000

    building it

--- src/ethereum/MinimalAccount.sol
+++ src/ethereum/MinimalAccount.sol
@@ -3,12 +3,36 @@ pragma solidity ^0.8.19;
 
 import {IAccount} from "lib/account-abstraction/contracts/interfaces/IAccount.sol";
 import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
-contract MinimalAccount is IAccount {
+import {Ownable} from "lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+contract MinimalAccount is IAccount,Ownable {
+    constructor() Ownable(msg.sender) {
+    }
    function validateUserOp(
         PackedUserOperation calldata userOp,
         bytes32 userOpHash,
         uint256 missingAccountFunds
     ) external returns (uint256 validationData){
+        _validateSignature(userOp, userOpHash);
+        _payPrefund(missingAccountFunds);
+    }
+    function _validateSignature(
+        PackedUserOperation calldata userOp,
+        bytes32 userOpHash
+    ) internal view returns (uint256 validationData) {
+        bytes32 ethSignedMessageHash = MessageHashUtils.toEthSignedMessageHash(userOpHash);
+        address signer = ECDSA.recover(ethSignedMessageHash, userOp.signature);
+        if(signer != owner()){
+return 1;
+        }
+        return 0;
         
+    }
+    function _payPrefund(uint256 missingAccountFunds) internal {
+        if (missingAccountFunds > 0) {
+            (bool success, ) = payable(msg.sender).call{value: missingAccountFunds,gas: type(uint256).max}("");
+            require(success, "prefund failed");
+        }
     }
 }
